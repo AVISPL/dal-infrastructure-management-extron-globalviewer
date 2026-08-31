0a7788362895ad9ad49967ac75ac1a9c8c1a21cb
RitikM-AVISPL <ritik.madaan@avispl.com>
2026-08-31 16:44:49 +0530

    SYAL-3303: Include controllers in total device count

--- src/main/java/com/avispl/symphony/dal/infrastructure/management/extron/globalviewer/GlobalViewerEnterpriseCommunicator.java
+++ src/main/java/com/avispl/symphony/dal/infrastructure/management/extron/globalviewer/GlobalViewerEnterpriseCommunicator.java
@@ -724,7 +724,7 @@ public class GlobalViewerEnterpriseCommunicator extends BaseCommunicator impleme
 			}
 
 			Map<String, String> dynamicStatistics = new HashMap<>();
-			dynamicStatistics.put(Constant.MONITORED_DEVICES_TOTAL, String.valueOf(cachedMonitoringDevice.size()));
+			dynamicStatistics.put(Constant.MONITORED_DEVICES_TOTAL, String.valueOf(cachedMonitoringDevice.size() + cachedControllers.size()));
 			dynamicStatistics.put(Constant.LAST_MONITORING_CYCLE_DURATION, String.valueOf(lastMonitoringCycleDuration));
 
 			this.localExtendedStatistics.setStatistics(statistics);
